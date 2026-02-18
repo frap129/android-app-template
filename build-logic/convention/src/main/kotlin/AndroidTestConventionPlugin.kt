bd1c801912cfe40cf2d6d9b2d1874cfea2ad2b2b
Joe Maples <joe@maples.dev>
2026-02-18 12:19:56 -0500

    refactor: Migrate to AGP Built in kotlin

--- build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
@@ -31,7 +31,6 @@ class AndroidTestConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.android.test")
-                apply("org.jetbrains.kotlin.android")
             }
 
             extensions.configure<TestExtension> {
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.dokka.gradle.DokkaTask
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinSymbolProcessor) apply false
     alias(libs.plugins.ktlint) apply false
